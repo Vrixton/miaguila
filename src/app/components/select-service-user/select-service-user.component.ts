81776d46415cf0d728ec606ebe584e28a9d22317
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2020-03-15 02:44:39 -0500

    select client

--- src/app/components/select-service-user/select-service-user.component.ts
+++ src/app/components/select-service-user/select-service-user.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-select-service-user',
-  templateUrl: './select-service-user.component.html',
-  styleUrls: ['./select-service-user.component.scss']
+  selector: "app-select-service-user",
+  templateUrl: "./select-service-user.component.html",
+  styleUrls: ["./select-service-user.component.scss"]
 })
 export class SelectServiceUserComponent implements OnInit {
+  client: number = 1;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  setClient(number) {
+    this.client = number;
   }
-
 }
